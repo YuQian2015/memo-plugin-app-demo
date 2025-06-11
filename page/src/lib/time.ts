765776783febc12e655cadb6091b776603ceda18
yuqian <757275030@qq.com>
2025-06-11 22:36:27 +0800

    feat: 移除不必要的文件，调整样式和页面结构，添加聊天功能和本地存储操作，优化转写内容展示逻辑。

--- page/src/lib/time.ts
+++ page/src/lib/time.ts
@@ -1,39 +1,3 @@
-
-// ChatGPT 生成代码 https://chat.openai.com/share/de98476e-de2c-4b40-89e6-cd7393401b4b
-// 示例用法
-// const seconds = 123.456;
-// const formattedTime = formatTime(seconds);
-// console.log(formattedTime); // 输出: 00:02:03.456
-
-export function formatTime(seconds: number): string {
-  const hours = Math.floor(seconds / 3600);
-  const minutes = Math.floor((seconds % 3600) / 60);
-  const remainingSeconds = (seconds % 3600) % 60;
-  const milliseconds = Math.floor((remainingSeconds % 1) * 1000);
-
-  const formattedHours = padNumber(hours);
-  const formattedMinutes = padNumber(minutes);
-  const formattedSeconds = padNumber(Math.floor(remainingSeconds));
-  const formattedMilliseconds = padNumber(milliseconds, 3);
-
-  return `${formattedHours}:${formattedMinutes}:${formattedSeconds}.${formattedMilliseconds}`;
-}
-export function formatTime2(seconds: number): string {
-  const hours = Math.floor(seconds / 3600);
-  const minutes = Math.floor((seconds % 3600) / 60);
-  const remainingSeconds = (seconds % 3600) % 60;
-
-  const formattedHours = padNumber(hours);
-  const formattedMinutes = padNumber(minutes);
-  const formattedSeconds = padNumber(Math.floor(remainingSeconds));
-
-  return `${formattedHours}:${formattedMinutes}:${formattedSeconds}`;
-}
-
-export function padNumber(num: number, length = 2): string {
-  return num.toString().padStart(length, "0");
-}
-
 export function convertToSeconds(str: string) {
   const parts = str.split(":"); // 用冒号分割字符串
   const hours = parts[parts.length - 3] ? +parts[parts.length - 3] : 0; // 取第一部分作为小时
--- page/src/pages/IndexPage.tsx
+++ page/src/pages/IndexPage.tsx
@@ -1,7 +1,13 @@
-import { useCallback, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { ScrollArea } from "@/components/ui/scroll-area";
-import PageComp from "./PageComp";
 import { Button } from "@/components/ui/button";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 
 import manifest from "../../../main/manifest.json";
 import type { ITranscriptFile } from "@aim-packages/iframe-ipc/dist/types";
@@ -51,14 +57,27 @@ export function transcriptFileToModel(transcriptFile: any) {
 
 function IndexPage() {
   const [transcriptionData, setTranscriptionData] = useState<ITranscriptFile | null>(null)
+
+  const [provider, setProvider] = useState("");
+  const [model, setModel] = useState("");
+  const [chatProvider, setChatProvider] = useState<Array<{
+    value: string,
+    label: string,
+    disabled?: boolean
+  }>>([]);
+  const [llmModels, setLlmModels] = useState<Array<{
+    value: string,
+    label: string,
+    disabled?: boolean
+  }>>([]);
+  const [prompt, setPrompt] = useState("");
+  const [generatedText, setGeneratedText] = useState<string>("");
   const queryParams = new URLSearchParams(window.location.search);
   const nId = queryParams.get('noteId') || undefined;
   const fId = queryParams.get('folderId') || undefined;
   const wId = queryParams.get('workspaceId') || undefined;
 
   const getTranscriptionData = useCallback(() => {
-    console.log("!23123123");
-
     window.AIM.transcriptionData('detail', {
       id: nId,
       workspaceId: wId,
@@ -72,11 +91,62 @@ function IndexPage() {
     })
   }, [nId, wId, fId])
 
+  useEffect(() => {
+    window.AIM.chat.getProviders().then((providers) => {
+      setChatProvider(providers)
+    });
+  }, []);
+
+  useEffect(() => {
+    if (provider) {
+      window.AIM.chat.getModels(provider as any).then((models) => {
+        if (models.length > 0) {
+          setLlmModels(models);
+        } else {
+          setLlmModels([]);
+        }
+      }
+      ).catch((err) => {
+        console.error(err);
+      });
+    }
+  }, [provider]);
+
+  useEffect(() => {
+    const m = llmModels.find((item) => item.value === model);
+    if (!m) {
+      setModel(llmModels[0]?.value || "");
+    }
+  }, [llmModels, model]);
+
+
+  useEffect(() => {
+    // @ts-ignore
+    function handleConvertProgress(e: any, data: any) {
+      if (data.type === "llm:chat:stream:start") {
+        setGeneratedText(data.data.text);
+      }
+      if (data.type === "llm:chat:stream:message") {
+        setGeneratedText((prev) => {
+          const accumulatedData = prev + data.data.text
+          return accumulatedData
+        });
+      }
+      if (data.type === "llm:chat:stream:completed") {
+        setGeneratedText(data.data.text);
+      }
+    }
+    window.AIM.handleMessage(handleConvertProgress, "AppPlugins");
+
+    return () => {
+      window.AIM.removeHandler("AppPlugins");
+    };
+  }, []);
 
   return (
     <div className="h-full">
-      <ScrollArea className="h-full p-2">
-        <div className="py-2">基本信息</div>
+      <ScrollArea className="h-full p-2 text-lg">
+        <div className="py-2 mt-2">基本信息</div>
         <div className="space-y-2">
           <div className="text-sm text-muted-foreground">当前转写ID：{nId}</div>
           <div className="text-sm text-muted-foreground">当前文件夹ID：{fId}</div>
@@ -84,7 +154,7 @@ function IndexPage() {
           <div className="text-sm text-muted-foreground">当前插件ID：{manifest.pluginId}</div>
         </div>
 
-        <div className="py-2">转写</div>
+        <div className="py-2 mt-2 text-lg">转写</div>
         <Button size={"sm"} variant={"outline"} onClick={getTranscriptionData}>
           获取内容
         </Button>
@@ -94,11 +164,88 @@ function IndexPage() {
           </ScrollArea>
         }
 
-        <div className="py-2">聊天</div>
-        <textarea className="w-full h-40 rounded-md box-border my-2 bg-background border outline-none"></textarea>
+        <div className="py-2 mt-2 text-lg">聊天</div>
+        <div className="text-sm text-muted-foreground">聊天服务</div>
+        <Select
+          value={provider}
+          onValueChange={(value) => {
+            setProvider(value);
+          }}
+        >
+          <SelectTrigger className="w-[180px]">
+            <SelectValue placeholder="选择AI服务" />
+          </SelectTrigger>
+          <SelectContent>
+            {
+              chatProvider.map((m) => (
+                <SelectItem key={m.value} value={m.value} disabled={m.disabled}>
+                  {m.label}
+                </SelectItem>
+              ))
+            }
+          </SelectContent>
+        </Select>
+        <div className="text-sm text-muted-foreground">AI模型</div>
+        <Select
+          value={model}
+          onValueChange={setModel}
+        >
+          <SelectTrigger className="w-[180px]">
+            <SelectValue placeholder="选择AI模型" />
+          </SelectTrigger>
+          <SelectContent>
+            {
+              llmModels.map((m) => (
+                <SelectItem key={m.value} value={m.value} disabled={m.disabled}>
+                  {m.label}
+                </SelectItem>
+              ))
+            }
+          </SelectContent>
+        </Select>
+        <textarea
+          value={prompt}
+          onChange={(e) => {
+            setPrompt(e.target.value)
+          }}
+          className="w-full h-20 rounded-md box-border my-2 bg-background border outline-none p-2"></textarea>
+        <Button size={"sm"} onClick={() => {
+          window.AIM.chat.chat({
+            model,
+            provider,
+            messages: [{
+              role: "user",
+              content: prompt
+            }]
+          });
+        }}>发送聊天内容</Button>
+        {
+          generatedText && <div>{generatedText}</div>
+        }
 
-        {transcriptionData && <PageComp currentFile={transcriptionData} />}
+        <div className="py-2 mt-2 text-lg">本地存储</div>
+        <div className="flex gap-2">
+          <Button size={"sm"} onClick={() => {
+            window.AIM.storage.setItem("data", {
+              text: generatedText || "test"
+            }, manifest.pluginId).then((res: any) => {
+              alert("保存成功")
+            })
+          }}>保存数据</Button>
+          <Button size={"sm"} onClick={() => {
+            window.AIM.storage.getItem("data", manifest.pluginId).then((res: any) => {
+              alert(res.text)
+            })
+          }}>获取数据</Button>
+        </div>
+
+        <div className="py-2 mt-2 text-lg">播放器</div>
+        <div className="flex gap-2">
+          <Button onClick={() => window.AIM.player.play()}>播放</Button>
+          <Button onClick={() => window.AIM.player.pause()}>暂停</Button>
+        </div>
       </ScrollArea>
+
     </div>
   )
 }
